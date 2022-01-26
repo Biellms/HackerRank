4cdeb6cad30632da127de2d2ca9cca6e405bd947
Gabriel Mendes <79611142+Biellms@users.noreply.github.com>
2022-01-26 15:43:52 -0300

    Update Station.sql

--- SQL/Station.sql
+++ SQL/Station.sql
@@ -24,7 +24,6 @@ SELECT DISTINCT CITY FROM STATION
 WHERE MOD(ID,2) = 0 ORDER BY CITY;
 
 /*Query the following two values from the STATION table:
-
 - The sum of all values in LAT_N rounded to a scale of 2 decimal places.
 - The sum of all values in LONG_W rounded to a scale of 2 decimal places.*/
 SELECT ROUND(sum(LAT_N), 2), ROUND(sum(LONG_W), 2)
