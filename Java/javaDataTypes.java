3ba8f756f1b3bae424fb8c13c024a81b445d0279
Biellms <biell.mendes8@gmail.com>
2022-02-02 15:26:23 -0300

    Att sentença Java DataTypes

--- Java/javaDataTypes.java
+++ Java/javaDataTypes.java
@@ -1,3 +1,44 @@
+/*Java has 8 primitive data types; char, boolean, byte, short, int, long, float, and double. For this exercise, we'll work with the primitives used to hold integer values (byte, short, int, and long):
+
+A byte is an 8-bit signed integer.
+A short is a 16-bit signed integer.
+An int is a 32-bit signed integer.
+A long is a 64-bit signed integer.
+
+Given an input integer, you must determine which primitive data types are capable of properly storing that input.
+
+Input Format:
+The first line contains an integer, T, denoting the number of test cases.
+Each test case, T, is comprised of a single line with an integer, n, which can be arbitrarily large or small.
+
+Output Format:
+For each input variable N and appropriate primitive dataType, you must determine if the given primitives are capable of storing it. 
+If yes, then print: N can be fitted in: * dataType
+
+Sample Input:
+    5
+    -150
+    150000
+    1500000000
+    213333333333333333333333333333333333
+    -100000000000000
+
+Sample Output:
+    -150 can be fitted in:
+    * short
+    * int
+    * long
+    150000 can be fitted in:
+    * int
+    * long
+    1500000000 can be fitted in:
+    * int
+    * long
+    213333333333333333333333333333333333 can't be fitted anywhere.
+    -100000000000000 can be fitted in:
+    * long
+*/
+
 import java.util.*;
 
 public class javaDataTypes {
