16c455fd0de261763ac3246a76411a7eaf2cc638
Biellms <biell.mendes8@gmail.com>
2022-02-03 14:42:58 -0300

    Add Java Method Overriding challenge

---
+++ Java/javaMethodOverriding.java
@@ -0,0 +1,52 @@
+/*
+Task
+Complete the code in your editor by writing an overridden getNumberOfTeamMembers method that prints the same statement as the superclass' getNumberOfTeamMembers method, 
+except that it replaces n with 11 (the number of players on a Soccer team).
+
+Output Format:
+When executed, your completed code should print the following:
+    Generic Sports
+    Each team has n players in Generic Sports
+    Soccer Class
+    Each team has 11 players in Soccer Class*/
+
+class Sports {
+
+    String getName() {
+        return "Generic Sports";
+    }
+    
+    void getNumberOfTeamMembers() {
+        System.out.println( "Each team has n players in " + getName() );
+    }
+}
+
+class Soccer extends Sports {
+
+    @Override
+    String getName() {
+        return "Soccer Class";
+    }
+
+    // Write your overridden getNumberOfTeamMembers method here
+    
+    void getNumberOfTeamMembers() {
+        System.out.println( "Each team has 11 players in Soccer Class ");
+    }
+
+}
+
+public class javaMethodOverriding {
+
+    public static void main(String []args) {
+
+        Sports c1 = new Sports();
+        Soccer c2 = new Soccer();
+
+        System.out.println(c1.getName());
+        c1.getNumberOfTeamMembers();
+        System.out.println(c2.getName());
+        c2.getNumberOfTeamMembers();
+
+    }
+}
