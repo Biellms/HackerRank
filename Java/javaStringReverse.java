6cb643da845a85bff00e5251113a261f34888b13
Biellms <biell.mendes8@gmail.com>
2022-02-03 15:13:14 -0300

    Add Java String Reverse

---
+++ Java/javaStringReverse.java
@@ -0,0 +1,34 @@
+/* Given a string A, print Yes if it is a palindrome, print No otherwise.
+
+Constraints:
+* A will consist at most 50 lower case english letters.
+
+
+Sample Input:
+    madam
+
+Sample Output:
+    Yes
+*/
+
+import java.util.*;
+
+public class javaStringReverse {
+    
+    public static void main(String[] args) {
+        
+        Scanner leia = new Scanner(System.in);
+
+        String A = leia.next();
+
+        String R = new StringBuilder(A).reverse().toString();
+
+        if (A.equals(R)) {
+            System.out.println("The word '"+A+"' is a palindrome!");
+        } else {
+            System.out.println("The word '"+A+"' is not a palindrome!");
+        }
+
+        leia.close();
+    }
+}
