abcf85fbc3587859dee4cb52df828f8867527e5e
Biellms <biell.mendes8@gmail.com>
2022-02-21 22:17:33 -0300

    Add Java Date And Time challenge

---
+++ Java/javaDateAndTime.java
@@ -0,0 +1,70 @@
+/*
+findDay has the following parameters:
+int: month
+int: day
+int: year
+
+Returns:
+string: the day of the week in capital letters
+
+Input Format:
+A single line of input containing the space separated month, day and year, respectively, in MM DD YY format.
+
+Sample Input:
+    08 05 2015
+
+Sample Output:
+    WEDNESDAY
+*/
+
+import java.io.*;
+import java.util.*;
+
+class Result {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+        
+        Calendar cal = Calendar.getInstance();
+
+        cal.set(Calendar.MONTH, month-1);
+        cal.set(Calendar.DAY_OF_MONTH, day);
+        cal.set(Calendar.YEAR, year);
+
+        String[] day_of_week = {"SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY","SATURDAY"};
+
+        return day_of_week[cal.get(Calendar.DAY_OF_WEEK)-1];
+    }
+
+}
+
+public class javaDateAndTime {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+        int day = Integer.parseInt(firstMultipleInput[1]);
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    
+    }
+}
